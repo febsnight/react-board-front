a8c8e34437086df02dc2eb3f6016e03caaaae580
user01 <febsnight@gmail.com>
2025-06-29 22:04:12 +0900

    게시글 글작성 추가

--- src/views/Board/Write/index.tsx
+++ src/views/Board/Write/index.tsx
@@ -1,7 +1,10 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css';
-import { useBoardStore } from '@/stores';
+import { useBoardStore, useLoginUserStore } from '@/stores';
 import { key } from 'localforage';
+import { useNavigate } from 'react-router-dom';
+import { MAIN_PATH } from '@/constant';
+import { useCookies } from 'react-cookie';
 
 //  component: 게시물 작성 화면
 function BoardWrite() {
@@ -19,6 +22,14 @@ function BoardWrite() {
   const { resetBoard } = useBoardStore();
   //  state: 게시물 이미지 미리보기 URL 상태
   const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  //  state: 로그인 유저 상태
+  const { loginUser } = useLoginUserStore();
+  //  state: 쿠키 상태
+  const [cookies, setCookies] = useCookies();
+
+  //  function: 네비게이트
+  const navigate = useNavigate();
 
   //  event handler: 제목 변경 이벤트 처리
   const onTitleChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -78,6 +89,11 @@ function BoardWrite() {
 
   //  effect: 마운트시 실행할 함수
   useEffect(() => {
+    const accessToken = cookies.accessToken;
+    if (!accessToken) {
+      navigate(MAIN_PATH());
+      return;
+    }
     resetBoard();
   }, []);
 
